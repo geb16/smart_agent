ce883f89cd25c532a88869245bd4a33d06ad4afb
Danyob16 <deg7@student.london.ac.uk>
2025-12-10 20:17:46 +0000

    Main agent runner has been  added

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+#from agent.planner import WorkflowPlanner
+from agent.planner_m16 import WorkflowPlanner
+from agent.agent_core import WorkflowAgent
+
+# whe can now create different planners and agents as needed
+planner = WorkflowPlanner()
+agent = WorkflowAgent(planner)
+
+print("🤖 Workflow Agent ready. Type 'exit' to quit.\n")
+
+while True:
+    user_in = input("You: ").strip()
+    if user_in.lower() in {"exit", "quit"}:
+        print("👋 Goodbye!")
+        break
+
+    out = agent.handle(user_in)
+    print("\n🤖:", out, "\n")
